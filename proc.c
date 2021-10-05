c1c74f657e8a15414fd1150d1990e165ebb32530
Tyler Persons <lx1263vl@smaug.mnstate.edu>
2021-10-04 21:39:39 -0500

    Lab 2 Finished--added ps and cp

--- proc.c
+++ proc.c
@@ -544,7 +544,7 @@ int cps() {
   //critical section
   cprintf("name\t pid\t state\t\n");
 
-  for (p = ptable.proc; p < ptable.prco[NPROC]; p++) {
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
     if (p -> state == SLEEPING) cprintf("%s \t %d \t SLEEPING \t \n", p->name, p->pid);
     else if (p->state == RUNNING) cprintf("%s \t %d \t RUNNING \t \n", p->name, p->pid);
     else if (p->state == RUNNABLE) cprintf("%s \t %d \t RUNNABLE \t \n", p->name, p->pid);
