023e2e6823a977e5cb6a2148ece973358cbf66a2
19tpersons <37720413+19tpersons@users.noreply.github.com>
2021-12-07 18:02:49 -0600

    Update trap.c

--- trap.c
+++ trap.c
@@ -62,7 +62,7 @@ trap(struct trapframe *tf)
     ideintr();
     //lapiceoi();
     cprintf("HERE >>");
-    cprintf(rcr2());
+    //cprintf(rcr2());
     lapiceoi();
     break;
   case T_IRQ0 + IRQ_IDE+1:
